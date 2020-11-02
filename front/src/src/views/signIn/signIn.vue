949fdc68b8ff60cb9a95fe0aa3e2c49da43c82e3
Alberto-Guerrero-Martinez <albertoprogramation95@gmail.com>
2020-11-03 00:27:06 +0100

    Update signIn.vue

--- front/src/src/views/signIn/signIn.vue
+++ front/src/src/views/signIn/signIn.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="signIn">
     <h1>sign in</h1>
-  </div>
+    <div id="">
+      <input type='text' id='name' placeholder='name'>
+      <input type='text' id='email' placeholder='email'>
+      <input type='text' id='confirmemail' placeholder='confirm email'>
+      <input type='text' id='password' placeholder='password'>
+      <input type='text' id='confirmpassword' placeholder='confirmpassword'>
+    </div>
+  </div> 
 </template>
 
 <script>
