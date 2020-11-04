dfafa8753d3ae61675a0db54ea274478234120bb
srXendo <david.martin.melgar@gmail.com>
2020-11-04 01:17:04 +0100

    add: signin validacion de email, contraseña y confirmacion de contraseña.

--- front/src/src/views/signIn/signIn.vue
+++ front/src/src/views/signIn/signIn.vue
@@ -1,5 +1,6 @@
 <script>
 import addUser from '../../services/user.service.js'
+
 export default {
   name: 'signIn',
   data: () => {
@@ -15,6 +16,7 @@ export default {
   },
   methods: {
     sendRegister: function () {
+      console.log()
       this.haveErrors()
       addUser(this.form)
     },
@@ -27,26 +29,25 @@ export default {
   <div>
     <h1>sign in</h1>
     <div id="register">
-      <input type='text' id='name' placeholder='name' v-model="form.name">
-      <div v-if="this.form.name ===''">
-        Rellena el espacio en blanco
-      </div>
-      <input type='email' id='email' placeholder='email' v-model="form.email">
-      <div v-if="this.form.email === ''">
-        Rellena el espacio en blanco
-      </div>
-      <input type='password' id='password' placeholder='password' v-model="form.password">
-      <div v-if="this.form.email === ''">
-        Rellena el espacio en blanco
-      </div>
-      <input type='password' id='confirmPassword' placeholder='confirm password' v-model="form.confirmPassword">
-      <div v-if="this.form.email === ''">
-        Rellena el espacio en blanco
-      </div>
-      <button type="button" id="sendRegister" @click="sendRegister">Register</button>
-      <div v-if='!this.sucess'>
-        error
+      <div style='display:flex; flex-direction:column;'>
+        <input type='text' id='name' placeholder='name' v-model="form.name">
+        <span v-if="this.form.name ===''" class="errorValidationText">* <br> Nombre de usuario requerido <br><br></span>
+        <input type='email' id='email' placeholder='email' v-model="form.email">
+        <span  v-if="this.form.email === ''" class="errorValidationText">Email requerido</span>
+        <span v-if="!(/^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/g.test(this.form.email))" class="errorValidationText"> * <br>formato de email invalido<br><br></span>
+        <input type='password' id='password' placeholder='password' v-model="form.password">
+        <span v-if="this.form.password === '' " class="errorValidationText">rellene el campo password</span>
+        <input type='password' id='confirmPassword' placeholder='confirm password' v-model="form.confirmPassword">
+        <span v-if="this.form.confirmPassword === '' " class="errorValidationText">Confirmacion de contraseña requerida</span>
+        <span class="errorValidationText" v-if="this.form.confirmPassword !== this.form.password || this.form.confirmPassword === ''">contraseña no es igual y confirma contraseña no es lo mismo <br><br></span>
+        <button type="button" id="sendRegister" @click="sendRegister">Register</button>
       </div>
     </div>
   </div>
+
 </template>
+<style>
+  .errorValidationText{
+    color:red;
+  }
+</style>
