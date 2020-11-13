9f81281814b19fba1030d7bb858e105599986e72
Alberto-Guerrero-Martinez <albertoprogramation95@gmail.com>
2020-11-13 17:08:31 +0100

    import mas players

--- back/api/user/user.js
+++ back/api/user/user.js
@@ -11,8 +11,13 @@ module.exports.start = async(app)=>{
     }, setSession)
     app.get('/user/getList', (req, res, next)=>{
         res.send(JSON.stringify([
-            {playerName: 'alberto', photoProfile: '?', position: 'ala', height: 1.80, years: 30, weight: 80, nation: 'spain'},
-            {playerName: 'david', photoProfile: '?', position: 'ala', height: 1.80, years: 26, weight: 80, nation: 'franch'}
+            {playerName: 'Alberto', photoProfile: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQYG2HvFOIpbog4D1M75FOpoK6QbUOy5rNa3Q&usqp=CAU', position: 'ala', height: 1.80, years: 30, weight: 80, nation: 'spain'},
+            {playerName: 'David', photoProfile: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQYG2HvFOIpbog4D1M75FOpoK6QbUOy5rNa3Q&usqp=CAU', position: 'ala', height: 1.80, years: 26, weight: 80, nation: 'franch'},
+            {playerName: 'Pol', photoProfile: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQYG2HvFOIpbog4D1M75FOpoK6QbUOy5rNa3Q&usqp=CAU', position: 'ala', height: 1.80, years: 26, weight: 80, nation: 'franch'},
+            {playerName: 'Marc', photoProfile: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQYG2HvFOIpbog4D1M75FOpoK6QbUOy5rNa3Q&usqp=CAU', position: 'ala', height: 1.80, years: 26, weight: 80, nation: 'franch'},
+            {playerName: 'Ian', photoProfile: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQYG2HvFOIpbog4D1M75FOpoK6QbUOy5rNa3Q&usqp=CAU', position: 'ala', height: 1.80, years: 26, weight: 80, nation: 'franch'},
+            {playerName: 'Arnau', photoProfile: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQYG2HvFOIpbog4D1M75FOpoK6QbUOy5rNa3Q&usqp=CAU', position: 'ala', height: 1.80, years: 26, weight: 80, nation: 'franch'}
+
         ])).status(200)
     })
 }
--- front/src/src/views/dashboard/dashboard.vue
+++ front/src/src/views/dashboard/dashboard.vue
@@ -17,17 +17,51 @@ export default {
 }
 
 </script>
-
 <template>
-    <div  v-if="playersList !== null">
-        <div id="playerlist" v-for="player in playersList" v-bind:key="player">
+    <div  v-if="playersList !== null" class="orderList">
+        <div id="playerlist" v-for="player in playersList" v-bind:key="player" class="listplayers">
             <p>Nombre:{{player.playerName}}</p>
-            <img v-bind:src="player.photoProfile" alt="">
-            <p>Edad:{{player.years}}</p>
-            <p>Peso:{{player.weight}}</p>
-            <p>Altura:{{player.height}}</p>
-            <p>Posicion:{{player.position}}</p>
-            <p>Categoria:{{getCategoryForYears(player.years)}}</p>
+            <img v-bind:src="player.photoProfile" alt="" class="photoprofile">
+            <div class="orderText">
+              <p>Edad:{{player.years}}</p>
+              <p>Peso:{{player.weight}}</p>
+              <p>Altura:{{player.height}}</p>
+              <p>Posicion:{{player.position}}</p>
+              <p>Categoria:{{getCategoryForYears(player.years)}}</p>
+            </div>
         </div>
     </div>
 </template>
+
+<style>
+.listplayers {
+  height: 6rem;
+  width: 30%;
+  background-color: rgb(241, 236, 236);
+  list-style-type: none;
+  text-align: center;
+  border-radius: 8%;
+  padding: 5% 57% 32% 6%;
+  transition: 0.3s;
+  cursor: pointer;
+}
+.listplayers:hover {
+box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.photoprofile {
+float: left;
+height: 9rem;
+border-radius: 50%;
+}
+.orderList{
+height: 27rem;
+display: grid;
+grid-template-columns: 33% 33% 33%;
+grid-template-rows: 100%;
+
+}
+.orderText{
+  float: right;
+}
+</style>
